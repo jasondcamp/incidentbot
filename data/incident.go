bcca1a9aefe85cebc01239ce7a8b098ac85b86e6
Jason Camp <jason.camp1@accesscfa.com>
2022-11-04 15:07:12 -0400

    Added stuff

---
+++ data/incident.go
@@ -0,0 +1,14 @@
+package data
+
+type Incident struct {
+	id			int
+	summary			string
+	opened_by	string
+	commander	string
+	manager	string
+	state			string
+	chat_room		string
+	created	string
+	start		string
+	end		string
+}
